bdc10ddfe59fd045854ccc5e95ce3904cc1a9341
ParsaSaeednia <p.parsa31@gmail.com>
2023-11-18 16:11:06 +0330

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return ( 
+		<Button className="m-3" variant={"destructive"} size={"sm"}>
+			Click Me
+		</Button>
+	);
+}
